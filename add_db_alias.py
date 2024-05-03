fe480ed18ce09bda33b91be1dc4ba6bb97881862
followarcane <batuhanbiter@hotmail.com>
2024-05-03 10:21:40 +0100

    Add files via upload
    First Commit

---
+++ add_db_alias.py
@@ -0,0 +1,39 @@
+import json
+from termcolor import colored
+
+def get_alias_data_from_user():
+    alias_name = input(colored('Enter alias name: ', 'magenta'))
+    hostname = input(colored('Enter hostname: ', 'magenta'))
+    port = input(colored('Enter port: ', 'magenta'))
+    region = input(colored('Enter region: ', 'magenta'))
+    profile = input(colored('Enter profile: ', 'magenta'))
+
+    return {
+        'name': alias_name,
+        'command': {
+            "hostname": hostname,
+            "port": int(port),
+            "region": region,
+            "profile": profile
+        }
+    }
+
+def add_alias_to_file(alias_data):
+    try:
+        with open('aliases.json', 'r') as f:
+            aliases = json.load(f)
+    except FileNotFoundError:
+        aliases = []
+
+    aliases.append(alias_data)
+
+    with open('aliases.json', 'w') as f:
+        json.dump(aliases, f, indent=4)
+
+def main():
+    alias_data = get_alias_data_from_user()
+    add_alias_to_file(alias_data)
+    print(colored('Alias added successfully!', 'green'))
+
+if __name__ == '__main__':
+    main()
---
+++ get_db_pass.py
@@ -0,0 +1,60 @@
+from termcolor import colored
+import pyperclip
+import configparser
+import os
+import boto3
+import json
+import botocore.exceptions
+
+
+def get_username():
+    config = configparser.ConfigParser()
+    if not os.path.exists('settings.ini'):
+        username_input = input(colored('Please enter your username: ', 'magenta'))
+        config['DEFAULT'] = {'username': username_input}
+        with open('settings.ini', 'w') as configfile:
+            config.write(configfile)
+    
+    config.read('settings.ini')
+    return config['DEFAULT']['username']
+
+
+def get_db_password_with_boto3(username, hostname, port, region, profile):
+    session = boto3.Session(profile_name=profile)
+    client = session.client('rds')
+    password = client.generate_db_auth_token(DBHostname=hostname, Port=port, DBUsername=username, Region=region)
+    return password
+
+
+def get_aliases_from_file():
+    with open('aliases.json', 'r') as f:
+        return json.load(f)
+
+
+def main():
+    try:
+        username = get_username()
+        alias_commands = get_aliases_from_file()
+
+        for index, alias_command in enumerate(alias_commands, start=1):
+            print(colored(f"{index}- {alias_command['name']}", 'cyan'))
+
+        choice = input(colored("Please choose a number from the list: ", 'magenta'))
+        selected_settings = alias_commands[int(choice) - 1]['command']
+        password = get_db_password_with_boto3(username, **selected_settings)
+        pyperclip.copy(password)
+        print(colored("Password has been stored in the clipboard for security. You can access it by pasting (CTRL+V or CMD+V).", 'yellow'))
+
+    except IndexError:
+        print(colored('Error: Invalid choice. Please enter a number corresponding to the list of aliases.', 'red'))
+    except FileNotFoundError:
+        print(colored('Error: File not found. Make sure the aliases.json file exists.', 'red'))
+    except json.decoder.JSONDecodeError:
+        print(colored('Error: Unable to parse the aliases.json file. Check if the file is a valid JSON.', 'red'))
+    except botocore.exceptions.ProfileNotFound:
+        print(colored('Error: The provided boto3 profile could not be found. Make sure the profile is configured properly.', 'red'))
+    except Exception as e:
+        print(colored(f'An unexpected error occurred: {str(e)}', 'red'))
+
+if __name__ == '__main__':
+    main()
